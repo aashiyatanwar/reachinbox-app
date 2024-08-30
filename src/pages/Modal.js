80ee16db621f1e105e4f81fa428a684d5fa1edf7
aashiyatanwar <tanwaraashiya12@gmail.com>
2024-08-30 11:33:00 +0530

    Rectify Code

--- src/pages/Modal.js
+++ src/pages/Modal.js
@@ -1,20 +1,3 @@
-// import React from "react";
-// import "../App.css"
-
-// const Modal = ({ isOpen, onClose, children }) => {
-//   if (!isOpen) return null;
-
-//   return (
-//     <div className="fixed top-0 left-0 w-full h-full bg-[#848484] bg-opacity-60 flex justify-center items-center">
-//       <div className="bg-black rounded-md shadow-md relative">
-//         <div>{children}</div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export { Modal };
-
 import React from "react";
 import "../App.css";
 
